7da19fe5fa6007d2898032aa28b9ac38ee671eab
FUKUYASU Naoki <naoki.fukuyasu@oit.ac.jp>
2022-04-12 10:40:23 +0900

    add git rules

---
+++ 70-git.mk
@@ -0,0 +1,46 @@
+#
+# ProGENv-git: Portable Programming Environment - MinGit
+#
+
+########################################################################
+
+#
+# ファイル／ディレクトリ構造
+#   ${BASE_DIR}
+#       +- MinGit : ${GIT_DIR}
+#       +- clinit-70-mingit.bat
+#
+
+GIT_DIR = MinGit
+
+CLINIT_GIT_SRC = clinit-70-git.src
+CLINIT_GIT_BAT = clinit-70-git.bat
+
+########################################################################
+
+GIT_VERSION = 2.35.1.2
+MINGIT_ZIP = ${DIST_DIR}/MinGit-${GIT_VERSION}-busybox-64-bit.zip
+
+########################################################################
+
+git: ${BASE_DIR}/${CLINIT_GIT_BAT}
+
+${BASE_DIR}/${GIT_DIR}/cmd/git.exe:
+	${MAKE} ${MAKE_FLAGS} ${BASE_DIR} 
+	${7Z} x ${MINGIT_ZIP} -o${BASE_DIR}/${GIT_DIR}
+
+${BASE_DIR}/${CLINIT_GIT_BAT}: ${CLINIT_GIT_SRC}
+	${MAKE} ${MAKE_FLAGS} cl
+	${MAKE} ${MAKE_FLAGS} ${BASE_DIR}/${GIT_DIR}/cmd/git.exe 
+	${RM} ${BASE_DIR}/${CLINIT_GIT_BAT}
+	${CAT} ${CLINIT_GIT_SRC} \
+	    | ${SED} -e 's|__GIT_DIR__|${GIT_DIR}|g' \
+	    > ${BASE_DIR}/${CLINIT_GIT_BAT}
+
+########################################################################
+
+clean::
+	${RM} ${BASE_DIR}/${CLINIT_GIT_BAT}
+
+clean::
+	${RM} -R ${BASE_DIR}/${GIT_DIR}
