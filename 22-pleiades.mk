db0021220808714d35ad4730d5be0464fd55a144
FUKUYASU Naoki <naoki.fukuyasu@oit.ac.jp>
2022-04-12 09:42:08 +0900

    update eclipse & pleiades version

--- 22-pleiades.mk
+++ 22-pleiades.mk
@@ -19,7 +19,7 @@ PLEIADES_DIR = pleiades
 
 ########################################################################
 
-PLEIADES_VERSION = 2021.03.28
+PLEIADES_VERSION = 2022.03.26
 PLEIADES_ZIP = ${DIST_DIR}/pleiades-win-${PLEIADES_VERSION}.zip
 
 ########################################################################
